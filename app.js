04fcb0314f0fc0c828d60d876652515a200bae02
Guilherme Chagas <guilhermecchagas1999@gmail.com>
2025-01-26 22:51:06 -0300

    addFriend function added

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 let friendsList = []
 
-function addFriend() {
-    friend = document.querySelector(".down-side__field-to-type").value;
-    return console.log(friend)
+function clearField() {
+    field = document.querySelector(".down-side__field-to-type");
+    field.value = "";
+    field.focus()
 }
+
+
+function addFriend() {
+    const friend = document.getElementById("down-side__field-to-type").value;
+    
+    if (friend) {
+        friendsList.push(friend)
+
+        const friendList = document.querySelector(".friendList");
+        const friendLine = document.createElement("li");
+
+        friendLine.textContent = friend;
+        friendList.appendChild(friendLine);
+
+        clearField()
+    } else {
+        alert("Please, type at least one name")
+    }
+}
+
+
