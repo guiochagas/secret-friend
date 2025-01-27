9dd353a18647161132fc276fc7b5ec058995d224
Guilherme Chagas <guilhermecchagas1999@gmail.com>
2025-01-26 23:41:50 -0300

    chooseFriend func and randomNumber added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-let friendsList = []
+let friendsList = [];
+
 
 function clearField() {
     field = document.querySelector(".down-side__field-to-type");
     field.value = "";
-    field.focus()
+    field.focus();
 }
 
 
@@ -11,7 +12,7 @@ function addFriend() {
     const friend = document.getElementById("down-side__field-to-type").value;
     
     if (friend) {
-        friendsList.push(friend)
+        friendsList.push(friend);
 
         const friendList = document.querySelector(".friendList");
         const friendLine = document.createElement("li");
@@ -19,10 +20,31 @@ function addFriend() {
         friendLine.textContent = friend;
         friendList.appendChild(friendLine);
 
-        clearField()
+        clearField();
     } else {
-        alert("Please, type at least one name")
+        alert("Please, type at least one name");
     }
 }
 
+function randomNumber() {
+    let listLenght = friendsList.length;
+    const number = parseInt(Math.random() * listLenght);
+    return number;
+}
 
+
+function chooseFriend() {
+    const chosenFriend = friendsList[randomNumber()];
+    const result = document.getElementById("down-side__friendResult");
+    result.innerHTML = chosenFriend;
+    return result
+}
+
+
+// Evento para que o botão "adicionar" seja executado ao teclar "Enter"
+const friendField = document.getElementById("down-side__field-to-type");
+friendField.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addFriend();  // Chama a função addFriend ao pressionar Enter
+    }
+});
